cbb3194b3b6ed19ec210881bb6d5d81a5a89034d
Nicolas Chaves <Nicolas@Nicolass-MacBook-Air.local>
2016-09-27 10:03:18 -0500

    Added basic models

---
+++ Isaacs/app/src/main/java/moviles/isaacs/com/isaacs/models/Content.java
@@ -0,0 +1,21 @@
+package moviles.isaacs.com.isaacs.models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nicolas on 9/27/16.
+ */
+
+public class Content {
+
+    public String data;
+
+    public String date_created;
+
+    public String type;
+
+    public ArrayList<Story> stories;
+
+    
+
+}
---
+++ Isaacs/app/src/main/java/moviles/isaacs/com/isaacs/models/Story.java
@@ -0,0 +1,21 @@
+package moviles.isaacs.com.isaacs.models;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nicolas on 9/27/16.
+ */
+
+public class Story {
+
+    public String brief;
+
+    public String date_created;
+
+    public String last_modified;
+
+    public String title;
+
+    public ArrayList<Content> contents;
+
+}
