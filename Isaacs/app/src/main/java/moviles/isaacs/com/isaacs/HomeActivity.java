a32f61ad54a97a036b9095da4a00c6fac73c60f8
Sebastian Florez <sfrsebastian@gmail.com>
2016-10-21 20:43:26 -0500

    visualizacion de pagina principal

--- Isaacs/app/src/main/java/moviles/isaacs/com/isaacs/HomeActivity.java
+++ Isaacs/app/src/main/java/moviles/isaacs/com/isaacs/HomeActivity.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private EditText editText;
+    private Button photoButton;
+    private Button audioButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        editText =  (EditText)findViewById(R.id.text_input_field);
+        photoButton = (Button)findViewById(R.id.photo_input_button);
+        audioButton = (Button)findViewById(R.id.audio_input_button);
     }
 
     public void goToDBTesting(View view) {
@@ -31,9 +39,6 @@ public class HomeActivity extends AppCompatActivity {
     public void goToInput(View view) {
         Intent goToInputActivity = new Intent(getApplicationContext(), InputActivity.class);
         switch (view.getId()) {
-            case R.id.text_input_button:
-                goToInputActivity.putExtra("INPUT_TYPE", "text");
-                break;
             case R.id.photo_input_button:
                 goToInputActivity.putExtra("INPUT_TYPE", "photo");
                 break;
